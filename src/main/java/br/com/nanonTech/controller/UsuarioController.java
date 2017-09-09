d39863c9129be0fa5459ec62f42ff33019222509
Rayssa Freitas <rayssafreitas13@gmail.com>
2017-09-09 12:38:11 -0300

    CRUD usuario Spring Boot

--- src/main/java/br/com/nanonTech/controller/UsuarioController.java
+++ src/main/java/br/com/nanonTech/controller/UsuarioController.java
@@ -2,7 +2,9 @@ package br.com.nanonTech.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,25 +13,36 @@ import br.com.nanonTech.model.Usuario;
 import br.com.nanonTech.repository.Usuarios;
 
 @Controller
-@RequestMapping("/usuarios")
+@RequestMapping
 public class UsuarioController {
-	
+
 	@Autowired
-	private Usuarios usuarios;
-	
-	
-	@GetMapping
+	private Usuarios usuariosRepository;
+
+	@GetMapping("/usuarios")
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaUsuarios");
-		modelAndView.addObject("usuarios", usuarios.findAll());
+		modelAndView.addObject("usuarios", usuariosRepository.findAll());
 		modelAndView.addObject(new Usuario());
 		return modelAndView;
 	}
-	
-	@PostMapping
+
+	@PostMapping("/usuarios")
 	public String salvar(Usuario usuario) {
-		this.usuarios.save(usuario);
+		this.usuariosRepository.save(usuario);
 		return "redirect:/usuarios";
+	}
+
+	@GetMapping("/usuarios/delete/{id}")
+	public String remove(@PathVariable(name = "id") Long id) {
+		this.usuariosRepository.delete(id);
+		return "redirect:/usuarios";
+	}
+	@GetMapping(path = "/usuarios/editar/{id}")
+	public String editar(Model model, @PathVariable(value = "id") Long id) {
+		model.addAttribute("usuario", usuariosRepository.findOne(id));
+		return "redirect:/usuarios";
+
 	}
 
 }
